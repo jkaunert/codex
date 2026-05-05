adb110e4a28153e4f733aa31fab22cbfe47cf3e3
joshua kaunert <44586402+jkaunert@users.noreply.github.com>
2026-05-05 18:27:31 -0500

    test: avoid fsevents watcher in desktop injection tests

--- codex-rs/core/tests/suite/plugins.rs
+++ codex-rs/core/tests/suite/plugins.rs
@@ -149,7 +149,7 @@ fn write_plugin_skill_plugin(home: &TempDir) -> std::path::PathBuf {
     skill_dir.join("SKILL.md")
 }
 
-#[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+#[tokio::test]
 async fn desktop_top_level_skill_injection_injects_apple_plugin_orchestrator() -> Result<()> {
     skip_if_no_network!(Ok(()));
 
@@ -215,7 +215,7 @@ async fn desktop_top_level_skill_injection_injects_apple_plugin_orchestrator() -
     Ok(())
 }
 
-#[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+#[tokio::test]
 async fn desktop_top_level_skill_injection_prepends_owner_for_explicit_downstream_route()
 -> Result<()> {
     skip_if_no_network!(Ok(()));
@@ -304,7 +304,7 @@ async fn desktop_top_level_skill_injection_prepends_owner_for_explicit_downstrea
     Ok(())
 }
 
-#[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+#[tokio::test]
 async fn desktop_top_level_skill_injection_keeps_focused_specialist_isolated() -> Result<()> {
     skip_if_no_network!(Ok(()));
 
